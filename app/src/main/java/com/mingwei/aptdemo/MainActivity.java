50c8202898efdc67bad07e4ca2c52c49c654929f
mingwei <721881283@qq.com>
2016-12-16 00:29:03 +0800

    gradle纠错

--- app/src/main/java/com/mingwei/aptdemo/MainActivity.java
+++ app/src/main/java/com/mingwei/aptdemo/MainActivity.java
@@ -2,9 +2,14 @@ package com.mingwei.aptdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.mingwe.demo.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.text)
+    private TextView mText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
