c037ab0c83bb84aaaeb4a2a37920aaac1a2ee98b
mingwei <721881283@qq.com>
2016-12-16 17:30:55 +0800

    finder

--- bindview-api/src/main/java/com/mingwei/myapi/finder/ActivityFInder.java
+++ bindview-api/src/main/java/com/mingwei/myapi/finder/ActivityFInder.java
@@ -1,7 +1,22 @@
 package com.mingwei.myapi.finder;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+
 /**
  * Created by mingwei on 12/16/16.
+ * CSDN:    http://blog.csdn.net/u013045971
+ * Github:  https://github.com/gumingwei
  */
-public class ActivityFInder {
+public class ActivityFinder implements Finder {
+    @Override
+    public Context getContext(Object source) {
+        return (Activity) source;
+    }
+
+    @Override
+    public View findView(Object source, int id) {
+        return ((Activity) source).findViewById(id);
+    }
 }
